4b6aff898948e43c1031dae03eba7bb6acae6888
skald <footmach@ya.ru>
2022-10-20 16:58:13 +0700

    fix

--- download_test.go
+++ download_test.go
@@ -12,7 +12,7 @@ import (
 func Test_tasker_downloading(t *testing.T) {
 	fmt.Println(t.Name())
 	Tr := new(Tasker).Init(runtime.NumCPU(), 512)
-	for i := 1; i <= 8; i++ {
+	for i := 1; i <= runtime.NumCPU(); i++ {
 		Tr.Add([]any{i, t}, downloading_tasker_tester, &Message{})
 		<-time.After(500 * time.Millisecond)
 	}
@@ -23,12 +23,12 @@ func Test_tasker_checker(t *testing.T) {
 	fmt.Println(t.Name())
 	Tr := new(Tasker).Init(runtime.NumCPU(), 512)
 	mes := &Message{}
-	for i := 1; i <= 8; i++ {
+	for i := 1; i <= runtime.NumCPU(); i++ {
 		Tr.Add([]any{i, t}, hash_files_tester, mes)
 		<-time.After(500 * time.Millisecond)
 	}
 	Tr.Wg.Wait()
-	for i := 1; i <= 8; i++ {
+	for i := 1; i <= runtime.NumCPU(); i++ {
 		os.RemoveAll(testid + sprintf("__%d", i))
 	}
 }
